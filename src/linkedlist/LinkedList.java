8d199b70f8bb9d331c83e42fe0b404154af310ef
equester <kvishwakarma@quadratyx.com>
2018-03-30 15:46:46 +0530

    Add mergeSort method

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,29 +1,38 @@
 package linkedlist;
 
 //Node class
-class Node{
+class Node {
 	int data;
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data + "]";
+	}
+
 	Node next;
-	Node(int d){
+
+	Node(int d) {
 		data = d;
 		next = null;
 	}
 }
+
 public class LinkedList {
-	Node head ;
-	
-	public boolean addAtFirst(int data){
-		try{
+	Node head;
+
+	public boolean addAtFirst(int data) {
+		try {
 			Node newNode = new Node(data);
 			newNode.next = head;
 			head = newNode;
-		}catch(Exception e){
+		} catch (Exception e) {
 			return false;
 		}
 		return true;
 	}
-	public int removeAtFirst(){
-		if(head==null){
+
+	public int removeAtFirst() {
+		if (head == null) {
 			return -1;
 		}
 		Node temp = null;
@@ -31,151 +40,161 @@ public class LinkedList {
 		head = head.next;
 		return temp.data;
 	}
-	public void addAtLast(int data){
+
+	public void addAtLast(int data) {
 		Node newNode = new Node(data);
-		
-		if(head==null){
+
+		if (head == null) {
 			head = newNode;
 			return;
 		}
 		Node temp = head;
-		
-		while(temp.next!=null){
+
+		while (temp.next != null) {
 			temp = temp.next;
 		}
 		temp.next = newNode;
 	}
-	public void removeAtLast(){
-		if(head==null){
+
+	public void removeAtLast() {
+		if (head == null) {
 			System.out.println("Nothing to delete");
 			return;
 		}
 		Node temp = head;
-		if(temp.next==null){
+		if (temp.next == null) {
 			head = null;
 			return;
 		}
-		
-		while(temp.next.next!=null){
+
+		while (temp.next.next != null) {
 			temp = temp.next;
 		}
 		temp.next = null;
 	}
-	public void deleteNodeByKey(int key){
+
+	public void deleteNodeByKey(int key) {
 		Node temp = head;
-		if(temp==null) {
+		if (temp == null) {
 			return;
 		}
-		if(temp.data==key){
+		if (temp.data == key) {
 			head = head.next;
 			return;
 		}
 		Node pre = null;
-		
-		while(temp!=null && temp.data != key){
+
+		while (temp != null && temp.data != key) {
 			pre = temp;
 			temp = temp.next;
 		}
-		if(temp!=null)
+		if (temp != null)
 			pre.next = temp.next;
 		else
 			System.out.println("\nKey not found to delete");
-		
+
 	}
-	public void printList(){
+
+	public void printList() {
 
 		Node temp = head;
-		while(temp!=null){
+		while (temp != null) {
 			System.out.print(temp.data);
-			if(temp.next!=null){
+			if (temp.next != null) {
 				System.out.print("->");
 			}
 			temp = temp.next;
 		}
 	}
-	public void deleteNodeAtIndex(int index){
-		if(head==null){
+
+	public void deleteNodeAtIndex(int index) {
+		if (head == null) {
 			return;
 		}
-		if(index==0){
+		if (index == 0) {
 			head = head.next;
 			return;
 		}
 		Node temp = head;
 		int pos = 0;
-		
-		while(temp!=null && pos < index-1){
+
+		while (temp != null && pos < index - 1) {
 			temp = temp.next;
 			pos++;
 		}
-		if(temp==null||temp.next==null){
+		if (temp == null || temp.next == null) {
 			System.out.println("Position is not in list range");
-			return ;
+			return;
 		}
 		temp.next = temp.next.next;
 	}
-	public int sizeItr(){
+
+	public int sizeItr() {
 		Node temp = head;
 		int size = 0;
-		while(temp!=null){
+		while (temp != null) {
 			size++;
 			temp = temp.next;
 		}
 		return size;
 	}
-	private int sizeR(Node node){
-		if(node==null){
+
+	private int sizeR(Node node) {
+		if (node == null) {
 			return 0;
 		}
-		return 1+sizeR(node.next);
+		return 1 + sizeR(node.next);
 	}
-	public int sizeRec(){
+
+	public int sizeRec() {
 		return sizeR(head);
 	}
-	public void swapByLink(int x, int y){
-		if(x==y){
+
+	public void swapByLink(int x, int y) {
+		if (x == y) {
 			System.out.println("Both elements are same can't swap");
 			return;
 		}
-		
+
 		Node preX = null, currX = head;
-		while(currX != null && currX.data != x){
+		while (currX != null && currX.data != x) {
 			preX = currX;
 			currX = currX.next;
 		}
-		
+
 		Node preY = null, currY = head;
-		while(currY != null && currY.data != y){
+		while (currY != null && currY.data != y) {
 			preY = currY;
 			currY = currY.next;
 		}
-		
-		if(currX==null||currY==null){
+
+		if (currX == null || currY == null) {
 			System.out.println("\nCan't swap....Either of two elements are not in list");
 			return;
 		}
-		
-		if(preX==null){
+
+		if (preX == null) {
 			head = currY;
-		}else{
+		} else {
 			preX.next = currY;
 		}
-		
-		if(preY==null){
+
+		if (preY == null) {
 			head = currX;
-		}else{
+		} else {
 			preY.next = currX;
 		}
 		Node temp = currX.next;
 		currX.next = currY.next;
 		currY.next = temp;
 	}
-	public void reverseItr(){
+
+	public void reverseItr() {
 		Node prev = null;
 		Node curr = head;
 		Node next = null;
-		
-		while(curr != null){
+
+		while (curr != null) {
 			next = curr.next;
 			curr.next = prev;
 			prev = curr;
@@ -183,125 +202,213 @@ public class LinkedList {
 		}
 		head = prev;
 	}
-	private Node reverseR(Node head){
-		if(head==null)
+
+	private Node reverseR(Node head) {
+		if (head == null)
 			return null;
-		if(head.next==null)
+		if (head.next == null)
 			return head;
-		
+
 		Node rest = head.next;
-		
+
 		head.next = null;
 		Node revRest = reverseR(rest);
 		rest.next = head;
-		
+
 		return revRest;
 	}
+
 	Node reverseUtil(Node curr, Node prev) {
-		if(curr==null)
+		if (curr == null)
 			return null;
-        /* If last node mark it head*/
-        if (curr.next == null) {
-            head = curr;
- 
-            /* Update next to prev node */
-            curr.next = prev;
-            return null;
-        }
- 
-        /* Save curr->next node for recursive call */
-        Node next1 = curr.next;
- 
-        /* and update next ..*/
-        curr.next = prev;
- 
-        reverseUtil(next1, curr);
-        return head;
-    }
-	public void reverseRec(){
-		head = reverseR(head);
-		//head = reverseUtil(head, null);
+		/* If last node mark it head */
+		if (curr.next == null) {
+			head = curr;
+
+			/* Update next to prev node */
+			curr.next = prev;
+			return null;
+		}
+
+		/* Save curr->next node for recursive call */
+		Node next1 = curr.next;
+
+		/* and update next .. */
+		curr.next = prev;
+
+		reverseUtil(next1, curr);
+		return head;
 	}
-	public Node mergeSortedList(Node head1, Node head2){
-		if(head1==null) return head2;
-		if(head2==null) return head1;
-		
-		if(head1.data < head2.data){
+
+	public void reverseRec() {
+		head = reverseR(head);
+		// head = reverseUtil(head, null);
+	}
+
+	public Node mergeSortedList(Node head1, Node head2) {
+		if (head1 == null)
+			return head2;
+		if (head2 == null)
+			return head1;
+
+		if (head1.data < head2.data) {
 			head1.next = mergeSortedList(head1.next, head2);
 			return head1;
-		}else{
+		} else {
 			head2.next = mergeSortedList(head2.next, head1);
 			return head2;
 		}
 	}
-	Node reverseKNodes(Node head, int k)
-    {
-       Node current = head;
-       Node next = null;
-       Node prev = null;
-        
-       int count = 0;
- 
-       /* Reverse first k nodes of linked list */
-       while (count < k && current != null) 
-       {
-           next = current.next;
-           current.next = prev;
-           prev = current;
-           current = next;
-           count++;
-       }
- 
-       /* next is now a pointer to (k+1)th node 
-          Recursively call for the list starting from current.
-          And make rest of the list as next of first node */
-       if (next != null) 
-          head.next = reverseKNodes(next, k);
- 
-       // prev is now head of input list
-       return prev;
-    }
-	public int getHeadData(){
+
+	Node reverseKNodes(Node head, int k) {
+		Node current = head;
+		Node next = null;
+		Node prev = null;
+
+		int count = 0;
+
+		/* Reverse first k nodes of linked list */
+		while (count < k && current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+			count++;
+		}
+
+		/*
+		 * next is now a pointer to (k+1)th node Recursively call for the list starting
+		 * from current. And make rest of the list as next of first node
+		 */
+		if (next != null)
+			head.next = reverseKNodes(next, k);
+
+		// prev is now head of input list
+		return prev;
+	}
+
+	public int getHeadData() {
 		return this.head.data;
 	}
-	public boolean isEmpty(){
+
+	public boolean isEmpty() {
 		return this.head == null;
 	}
-	public static void main(String[] args){
-		LinkedList l1 = new LinkedList();
-		l1.addAtFirst(1);
-		l1.addAtLast(5);
-		l1.addAtLast(8);
-		l1.addAtLast(10);
-		l1.addAtLast(11);
-		l1.printList();
-		l1.reverseRec();
-		System.out.println();
-		l1.printList();
-//		System.out.println();
-//		LinkedList l2 = new LinkedList();
-//		l2.addAtFirst(2);
-//		l2.addAtLast(3);
-//		l2.addAtLast(6);
-//		l2.addAtLast(7);
-//		l2.addAtLast(12);
-//		l2.printList();
-//		System.out.println();
-//		Node newHead = l1.mergeSortedList(l1.head,l2.head);
-//		l1.head = newHead;
-//		l1.printList();
-//		System.out.println();
-//		newHead = l1.reverseKNodes(l1.head,3);
-//		l1.head = newHead;
-//		l1.printList();
-		//ll.swapByLink(8, 4);
-		//System.out.println();
-		/*ll.reverseRec();
-		
-		ll.printList();
-		
-		System.out.println();
-		ll.reverseItr();
-		ll.printList();*/
+
+	public Node nThNodeFromLast(int n) {
+		// O(N) complexity, where N = size of linked list
+		Node temp = head, nThNode = head;
+		int count = 0;
+		while (temp != null && count < n) {
+			temp = temp.next;
+			count++;
+		}
+		if (temp != null) {
+			while (temp != null) {
+				temp = temp.next;
+				nThNode = nThNode.next;
+			}
+		}
+		return nThNode;
+	}
+
+	public boolean hasLoop() {
+		Node fast = head;
+		Node slow = head;
+
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				System.out.println("There is a loop in linked list");
+				removeLoop(slow);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void removeLoop(Node loopNode) {
+		Node ptr1 = loopNode;
+		Node ptr2 = loopNode;
+
+		// Count the number of nodes in loop
+		int loopCount = 1;
+		while (ptr1.next != ptr2) {
+			ptr1 = ptr1.next;
+			loopCount++;
+		}
+		// Fix one pointer to head and other pointer to k nodes after head
+		ptr1 = head;
+		ptr2 = head;
+
+		for (int i = 0; i < loopCount; i++) {
+			ptr2 = ptr2.next;
+		}
+
+		/*
+		 * Move both pointers at the same pace, they will meet at loop starting node
+		 */
+
+		while (ptr1 != ptr2) {
+			ptr1 = ptr1.next;
+			ptr2 = ptr2.next;
+		}
+		// Getting pointer to the last node in loop
+		while (ptr2.next != ptr1) {
+			ptr2 = ptr2.next;
+		}
+		/*
+		 * Now ptr1 is at first node of loop and ptr2 is at last node of loop Disconnect
+		 * this loop
+		 */
+		ptr2.next = null;
+	}
+
+	/***
+	 * This method returns object of middle node
+	 * 
+	 * @return
+	 */
+	Node getMiddleNode(Node head) {
+		if (head == null)
+			return head;
+		Node fast = head.next;
+		Node slow = head;
+		// Move fast by two and slow ptr by one
+		// Finally slow will point to middle node
+		while (fast != null) {
+			fast = fast.next;
+			if (fast != null) {
+				slow = slow.next;
+				fast = fast.next;
+			}
+		}
+		return slow;
+	}
+
+	private Node mergeSort(Node head) {
+		if (head == null || head.next == null)
+			return head;
+
+		// Split current list into two halves
+		Node middleNode = getMiddleNode(head);
+		Node nextOfMiddleNode = middleNode.next;
+
+		// Disconnect middle node
+		middleNode.next = null;
+
+		// Apply mergeSort on left sub-list
+		Node leftList = mergeSort(head);
+		// Apply mergeSort on right sub-list
+		Node rightList = mergeSort(nextOfMiddleNode);
+
+		// Merge both sub-list
+		Node sortedList = mergeSortedList(leftList, rightList);
+		return sortedList;
+	}
+
+	public void sort() {
+		head = mergeSort(head);
 	}
 }
---
+++ src/linkedlist/LinkedListTest.java
@@ -0,0 +1,32 @@
+package linkedlist;
+
+import java.util.Scanner;
+
+public class LinkedListTest {
+
+	public static void main(String[] args){
+		LinkedList l1 = new LinkedList();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter elements for linked list");
+		int no;
+		while((no = sc.nextInt())!=-1) {
+			l1.addAtLast(no);
+		}
+		l1.printList();
+		System.out.println();
+		l1.sort();
+		l1.printList();
+		/*LinkedList list = new LinkedList();
+        list.head = new Node(1);
+        list.head.next = new Node(2);
+        list.head.next.next = new Node(3);
+        list.head.next.next.next = new Node(4);
+        list.head.next.next.next.next = new Node(5);
+        list.head.next.next.next.next.next = new Node(6);
+        //creating loop
+        list.head.next.next.next.next.next.next = list.head;
+        list.hasLoop();
+        list.printList();*/
+	}
+
+}
