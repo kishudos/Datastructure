11683c10158f5d672721b7eedf4f60d5ff546826
equester <kvishwakarma@quadratyx.com>
2018-04-24 13:57:32 +0530

    Add new implementation

--- src/queue/CircularQueue.java
+++ src/queue/CircularQueue.java
@@ -1,96 +1,84 @@
 package queue;
 
 public class CircularQueue {
-	private int front, rear, capacity;
-	private int[] queue = null;
+	private int front, rear;
+	private int capacity;
+	private int[] queue;
 
-	public CircularQueue(int capacity) {
-		this.capacity = capacity;
+	public CircularQueue(int size) {
 		this.front = this.rear = -1;
-		this.queue = new int[capacity];
-	}
-
-	public boolean isFull() {
-		return this.front == ((this.rear + 1) % this.capacity);
+		this.capacity = size;
+		this.queue = new int[this.capacity];
 	}
 
 	public boolean isEmpty() {
-		return (this.front == this.rear) && (this.rear == -1);
+		return (this.front == this.rear) && this.rear == -1;
 	}
 
-	public void enQ(int data) {
-		if (!this.isFull()) {
-			this.rear = (this.rear + 1) % this.capacity;
+	public boolean isFull() {
+		return this.front == (this.rear + 1) % this.capacity;
+	}
+	public void enqueue(int data) {
+		if(!isFull()) {
+			this.rear = (this.rear+1)%this.capacity;
 			this.queue[this.rear] = data;
-
-			if (this.front == -1)
+			if(this.front==-1)
 				this.front = 0;
-		} else {
+		}else {
 			System.out.println("Queue is full");
 		}
 	}
-
-	public int deQ() {
-		int data = -1;
-		if (!isEmpty()) {
-			data = this.queue[this.front];
-
-			if (this.front == this.rear)
+	public int dequeue() {
+		int item = -1;
+		if(!isEmpty()) {
+			item = this.queue[this.front];
+			if(this.front==this.rear) {
 				this.front = this.rear = -1;
-			else
-				this.front = (this.front + 1) % this.capacity;
-		} else {
-			System.out.println("Queue is already empty..!");
-		}
-		return data;
-	}
-
-	public int getFront() {
-		return this.queue[this.front];
-	}
-
-	public int getRear() {
-		return this.queue[this.rear];
-	}
-
-	public void printQ() {
-		int start = this.front, end = this.rear;
-		if (start != -1) {
-			while (start != end) {
-				System.out.print(this.queue[start] + " ");
-				start = (start + 1) % this.capacity;
+			}else {
+				this.front = (this.front+1)%this.capacity;
 			}
-			System.out.println();
+		}else {
+			System.out.println("Queue is empty");
 		}
+		return item;
 	}
-
+	public int getFront() {
+		int item=-1;
+		if(!isEmpty()) {
+			item = this.queue[this.front];
+		}else {
+			System.out.println("Empty queue");
+		}
+		return item;
+	}
+	public int getRear() {
+		int item = -1;
+		if(!isEmpty()) {
+			item = this.queue[this.rear];
+		}else {
+			System.out.println("Empty queue");
+		}
+		return item;
+	}
 	public static void main(String[] args) {
-		CircularQueue q = new CircularQueue(5);
-		q.printQ();
-		q.deQ();
-		q.enQ(1);
-		System.out.println("front-" + q.getFront());
-		System.out.println("Rear- " + q.getRear());
-		q.enQ(2);
-		System.out.println("front-" + q.getFront());
-		System.out.println("Rear- " + q.getRear());
-		q.enQ(3);
-		System.out.println("front-" + q.getFront());
-		System.out.println("Rear- " + q.getRear());
-		q.enQ(4);
-		System.out.println("front-" + q.getFront());
-		System.out.println("Rear- " + q.getRear());
-		q.enQ(5);
-		System.out.println("front-" + q.getFront());
-		System.out.println("Rear- " + q.getRear());
-		q.enQ(6);
-		System.out.println("front-" + q.getFront());
-		System.out.println("Rear- " + q.getRear());
-		System.out.println("Deleted-" + q.deQ());
-		System.out.println("front-" + q.getFront());
-		System.out.println("Rear- " + q.getRear());
-		q.enQ(6);
-		System.out.println("front-" + q.getFront());
-		System.out.println("Rear- " + q.getRear());
+		CircularQueue cQ = new CircularQueue(5);
+		System.out.println("Front - "+cQ.getFront());
+		System.out.println("Rear - "+cQ.getRear());
+		cQ.enqueue(1);
+		System.out.println("Front - "+cQ.getFront());
+		System.out.println("Rear - "+cQ.getRear());
+		cQ.enqueue(2);
+		System.out.println("Front - "+cQ.getFront());
+		System.out.println("Rear - "+cQ.getRear());
+		cQ.enqueue(3);
+		System.out.println("Front - "+cQ.getFront());
+		System.out.println("Rear - "+cQ.getRear());
+		cQ.enqueue(4);
+		System.out.println("Front - "+cQ.getFront());
+		System.out.println("Rear - "+cQ.getRear());
+		cQ.enqueue(5);
+		System.out.println(cQ.dequeue());
+		System.out.println("Front - "+cQ.getFront());
+		System.out.println("Rear - "+cQ.getRear());
 	}
 }
